03b3f579dd3e42fefcbd7976332aeaaab58ac33e
Hal Rosenstock <hal@mellanox.com>
2014-01-16 17:46:23 +0200

    access/ssa_prdb.c: Cosmetic changes
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- access/src/ssa_prdb.c
+++ access/src/ssa_prdb.c
@@ -31,13 +31,12 @@
  *
  */
 
-
 #include <ssa_db.h>
 #include <ssa_prdb.h>
 #include <asm/byteorder.h>
 
 static const struct db_table_def def_tbl[] = {
-	{ DBT_DEF_VERSION, sizeof(struct db_table_def), DBT_TYPE_DATA, 0, { 0,SSA_PR_TABLE_ID, 0 },
+	{ DBT_DEF_VERSION, sizeof(struct db_table_def), DBT_TYPE_DATA, 0, { 0, SSA_PR_TABLE_ID, 0 },
 		"PR", __constant_htonl(sizeof(struct ep_pr_tbl_rec)), 0 },
 	{ DBT_DEF_VERSION, sizeof(struct db_table_def), DBT_TYPE_DEF, 0, { 0, SSA_PR_TABLE_ID_FIELD_DEF , 0 },
 		"PR fields", __constant_htonl(sizeof(struct db_field_def)), __constant_htonl(SSA_PR_TABLE_ID) },
@@ -55,7 +54,7 @@ static const struct db_dataset field_dataset_tbl[] = {
 };
 
 static const struct db_field_def field_tbl[] = {
-	{ DBF_DEF_VERSION, 0, DBF_TYPE_NET64, 0, { 0,SSA_PR_TABLE_ID_FIELD_DEF,SSA_PR_FIELD_ID_PR_DGUID}, "guid", __constant_htonl(64), 0 },
+	{ DBF_DEF_VERSION, 0, DBF_TYPE_NET64, 0, { 0, SSA_PR_TABLE_ID_FIELD_DEF,SSA_PR_FIELD_ID_PR_DGUID}, "guid", __constant_htonl(64), 0 },
 	{ DBF_DEF_VERSION, 0, DBF_TYPE_NET16, 0, { 0, SSA_PR_TABLE_ID_FIELD_DEF, SSA_PR_FIELD_ID_PR_DLID }, "lid", __constant_htonl(16), __constant_htonl(64) },
 	{ DBF_DEF_VERSION, 0, DBF_TYPE_NET16, 0, { 0, SSA_PR_TABLE_ID_FIELD_DEF, SSA_PR_FIELD_ID_PR_PK }, "pk", __constant_htonl(16), __constant_htonl(80) },
 	{ DBF_DEF_VERSION, 0, DBF_TYPE_U8, 0, { 0, SSA_PR_TABLE_ID_FIELD_DEF, SSA_PR_FIELD_ID_PR_MTU }, "mtu", __constant_htonl(8), __constant_htonl(96) },
@@ -65,7 +64,6 @@ static const struct db_field_def field_tbl[] = {
 	{ DB_VERSION_INVALID }
 };
 
-
 /** =========================================================================
  */
 struct ssa_db  *ssa_prdb_create(uint64_t num_recs)
@@ -73,18 +71,17 @@ struct ssa_db  *ssa_prdb_create(uint64_t num_recs)
 	struct ssa_db *p_ssa_db = NULL;
 	uint64_t num_recs_arr[SSA_PR_TABLE_ID_MAX] = {};
 	uint64_t num_field_recs_arr[SSA_PR_TABLE_ID_MAX] = {};
-	size_t recs_size_arr[SSA_PR_TABLE_ID_MAX] ={};
-
+	size_t recs_size_arr[SSA_PR_TABLE_ID_MAX] = {};
 
 	num_recs_arr[SSA_PR_TABLE_ID] = num_recs;
 	recs_size_arr[SSA_PR_TABLE_ID] = sizeof(struct ep_pr_tbl_rec);
 	num_field_recs_arr[SSA_PR_TABLE_ID] = SSA_PR_FIELDS_ID_MAX;
 
-	p_ssa_db = ssa_db_create(num_recs_arr, recs_size_arr, num_field_recs_arr, SSA_PR_TABLE_ID_MAX);
+	p_ssa_db = ssa_db_create(num_recs_arr, recs_size_arr,
+				 num_field_recs_arr, SSA_PR_TABLE_ID_MAX);
 
-	ssa_db_init(p_ssa_db, "PRDB", 10 /*just some db_id */, def_tbl, dataset_tbl,
-		    field_dataset_tbl, field_tbl);
+	ssa_db_init(p_ssa_db, "PRDB", 10 /*just some db_id */, def_tbl,
+		    dataset_tbl, field_dataset_tbl, field_tbl);
 
 	return p_ssa_db;
 }
-
