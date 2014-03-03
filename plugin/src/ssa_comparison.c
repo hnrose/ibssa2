11ce5535d91f73cbd1df9f2483d47bf2e9612265
Ilya Nelkenbaum <ilyan@mellanox.com>
2014-03-03 17:06:52 +0200

    plugin/ssa_comparison.c: Eliminate duplicated code handling LFT records
    Signed-off-by: Ilya Nelkenbaum <ilyan@mellanox.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- plugin/src/ssa_comparison.c
+++ plugin/src/ssa_comparison.c
@@ -1381,16 +1381,16 @@ ssa_db_compare(struct ssa_database * ssa_db, uint64_t epoch_prev)
 		tbl_changed[SSA_TABLE_ID_LFT_TOP] = TRUE;
 	}
 
-	if (!smdb_deltas)
-		goto force_full;
-	if (first) {
+	if (!smdb_deltas || first) {
 		ep_lft_block_qmap_copy(&p_ssa_db_diff->ep_lft_block_tbl, &p_ssa_db_diff->p_smdb->p_db_tables[SSA_TABLE_ID_LFT_BLOCK],
 				     p_ssa_db_diff->p_smdb->pp_tables[SSA_TABLE_ID_LFT_BLOCK], &ssa_db->p_lft_db->ep_db_lft_block_tbl,
 				     ssa_db->p_lft_db->p_db_lft_block_tbl);
 		ep_lft_top_qmap_copy(&p_ssa_db_diff->ep_lft_top_tbl, &p_ssa_db_diff->p_smdb->p_db_tables[SSA_TABLE_ID_LFT_TOP],
 				     p_ssa_db_diff->p_smdb->pp_tables[SSA_TABLE_ID_LFT_TOP], &ssa_db->p_lft_db->ep_db_lft_top_tbl,
 				     ssa_db->p_lft_db->p_db_lft_top_tbl);
-	} else {
+	}
+
+	if (!first) {
 		ep_lft_block_qmap_copy(&p_ssa_db_diff->ep_lft_block_tbl, &p_ssa_db_diff->p_smdb->p_db_tables[SSA_TABLE_ID_LFT_BLOCK],
 				     p_ssa_db_diff->p_smdb->pp_tables[SSA_TABLE_ID_LFT_BLOCK], &ssa_db->p_lft_db->ep_dump_lft_block_tbl,
 				     ssa_db->p_lft_db->p_dump_lft_block_tbl);
@@ -1433,58 +1433,7 @@ ssa_db_compare(struct ssa_database * ssa_db, uint64_t epoch_prev)
 		ep_qmap_clear(&ssa_db->p_lft_db->ep_dump_lft_block_tbl);
 		ep_qmap_clear(&ssa_db->p_lft_db->ep_dump_lft_top_tbl);
 	}
-	goto force_done;
 
-force_full:
-	ep_lft_block_qmap_copy(&p_ssa_db_diff->ep_lft_block_tbl, &p_ssa_db_diff->p_smdb->p_db_tables[SSA_TABLE_ID_LFT_BLOCK],
-			     p_ssa_db_diff->p_smdb->pp_tables[SSA_TABLE_ID_LFT_BLOCK], &ssa_db->p_lft_db->ep_db_lft_block_tbl,
-			     ssa_db->p_lft_db->p_db_lft_block_tbl);
-	ep_lft_top_qmap_copy(&p_ssa_db_diff->ep_lft_top_tbl, &p_ssa_db_diff->p_smdb->p_db_tables[SSA_TABLE_ID_LFT_TOP],
-			     p_ssa_db_diff->p_smdb->pp_tables[SSA_TABLE_ID_LFT_TOP], &ssa_db->p_lft_db->ep_db_lft_top_tbl,
-			     ssa_db->p_lft_db->p_db_lft_top_tbl);
-	ep_lft_block_qmap_copy(&p_ssa_db_diff->ep_lft_block_tbl, &p_ssa_db_diff->p_smdb->p_db_tables[SSA_TABLE_ID_LFT_BLOCK],
-			     p_ssa_db_diff->p_smdb->pp_tables[SSA_TABLE_ID_LFT_BLOCK], &ssa_db->p_lft_db->ep_dump_lft_block_tbl,
-			     ssa_db->p_lft_db->p_dump_lft_block_tbl);
-	ep_lft_top_qmap_copy(&p_ssa_db_diff->ep_lft_top_tbl, &p_ssa_db_diff->p_smdb->p_db_tables[SSA_TABLE_ID_LFT_TOP],
-			     p_ssa_db_diff->p_smdb->pp_tables[SSA_TABLE_ID_LFT_TOP], &ssa_db->p_lft_db->ep_dump_lft_top_tbl,
-			     ssa_db->p_lft_db->p_dump_lft_top_tbl);
-
-	if (cl_qmap_count(&ssa_db->p_lft_db->ep_dump_lft_top_tbl))
-		tbl_changed[SSA_TABLE_ID_LFT_TOP] = TRUE;
-
-	if (cl_qmap_count(&ssa_db->p_lft_db->ep_dump_lft_block_tbl))
-		tbl_changed[SSA_TABLE_ID_LFT_BLOCK] = TRUE;
-
-	new_recs = ssa_db_diff_new_qmap_recs(&ssa_db->p_lft_db->ep_db_lft_top_tbl,
-					     &ssa_db->p_lft_db->ep_dump_lft_top_tbl);
-	if (new_recs > 0) {
-		ssa_db->p_lft_db->p_db_lft_top_tbl = (struct ep_lft_top_tbl_rec *)
-				realloc(&ssa_db->p_lft_db->p_db_lft_top_tbl[0],
-					(cl_qmap_count(&ssa_db->p_lft_db->ep_db_lft_top_tbl) + new_recs) *
-					 sizeof(*ssa_db->p_lft_db->p_db_lft_top_tbl));
-	}
-
-	new_recs = ssa_db_diff_new_qmap_recs(&ssa_db->p_lft_db->ep_db_lft_block_tbl,
-					     &ssa_db->p_lft_db->ep_dump_lft_block_tbl);
-	if (new_recs > 0) {
-		ssa_db->p_lft_db->p_db_lft_block_tbl = (struct ep_lft_block_tbl_rec *)
-				realloc(&ssa_db->p_lft_db->p_db_lft_block_tbl[0],
-					(cl_qmap_count(&ssa_db->p_lft_db->ep_db_lft_block_tbl) + new_recs) *
-					 sizeof(*ssa_db->p_lft_db->p_db_lft_block_tbl));
-	}
-
-	/* Apply LFT block / top changes on existing LFT database */
-	ep_lft_block_qmap_copy(&ssa_db->p_lft_db->ep_db_lft_block_tbl, NULL,
-			     ssa_db->p_lft_db->p_db_lft_block_tbl, &ssa_db->p_lft_db->ep_dump_lft_block_tbl,
-			     ssa_db->p_lft_db->p_dump_lft_block_tbl);
-	ep_lft_top_qmap_copy(&ssa_db->p_lft_db->ep_db_lft_top_tbl, NULL,
-			     ssa_db->p_lft_db->p_db_lft_top_tbl, &ssa_db->p_lft_db->ep_dump_lft_top_tbl,
-			     ssa_db->p_lft_db->p_dump_lft_top_tbl);
-	/* Clear LFT dump data */
-	ep_qmap_clear(&ssa_db->p_lft_db->ep_dump_lft_block_tbl);
-	ep_qmap_clear(&ssa_db->p_lft_db->ep_dump_lft_top_tbl);
-
-force_done:
 	if (!p_ssa_db_diff->dirty) {
                 ssa_log(SSA_LOG_VERBOSE, "SMDB was not changed\n");
                 ssa_db_diff_destroy(p_ssa_db_diff);
