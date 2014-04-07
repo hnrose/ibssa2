63d90abedb65ccf795d9d5f4752ec650eae5d40c
Ilya Nelkenbaum <ilyan@mellanox.com>
2014-04-07 15:58:33 +0300

    plugin/core.c: Add core plugin configuration file name to log
    Signed-off-by: Ilya Nelkenbaum <ilyan@mellanox.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- plugin/src/core.c
+++ plugin/src/core.c
@@ -1000,6 +1000,7 @@ static const char *core_node_type_str(int node_type)
 static void core_log_options(void)
 {
 	ssa_log_options();
+	ssa_log(SSA_LOG_DEFAULT, "config file %s\n", opts_file);
 	ssa_log(SSA_LOG_DEFAULT, "lock file %s\n", lock_file);
 	ssa_log(SSA_LOG_DEFAULT, "node type %d (%s)\n", node_type,
 		core_node_type_str(node_type));
