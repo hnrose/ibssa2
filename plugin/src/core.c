hnrose/ibssa2

commit 9ab787a7d9670825ce8d628993993a40c65da2bd

plugin/core.c: Move duplicated code into a single method
Add ssa_extract_update_ready_process() method for
processing SSA_DB_UPDATE_READY messages in extract
handler to factor out duplicated code.

Signed-off-by: Ilya Nelkenbaum <ilyan@mellanox.com>
Signed-off-by: Hal Rosenstock <hal@mellanox.com>

plugin/src/core.c
@@ -941,6 +941,20 @@ static void core_extract_db(osm_opensm_t *p_osm)
 	first = 0;
 }
 
+#ifndef SIM_SUPPORT
+static void ssa_extract_update_ready_process(osm_opensm_t *p_osm,
+					     struct ref_count_obj *p_smdb,
+					     int *outstanding_count)
+{
+	if (*outstanding_count > 0) {
+		if (--(*outstanding_count) == 0) {
+			core_extract_db(p_osm);
+			ssa_extract_db_update(p_smdb);
+		}
+	}
+}
+#endif
+
 static void *core_extract_handler(void *context)
 {
 	struct ssa_extract_data *p_extract_data = (struct ssa_extract_data *) context;
@@ -1056,12 +1070,8 @@ else ssa_log(SSA_LOG_DEFAULT, "extract event with extract already pending\n");
 #ifndef SIM_SUPPORT
 			case SSA_DB_UPDATE_READY:
 ssa_log(SSA_LOG_DEFAULT, "SSA_DB_UPDATE_READY from access with outstanding count %d\n", outstanding_count);
-				if (outstanding_count > 0) {
-					if (--outstanding_count == 0) {
-						core_extract_db(p_osm);
-						ssa_extract_db_update(ssa_db_diff->p_smdb);
-					}
-				}
+				ssa_extract_update_ready_process(p_osm, ssa_db_diff->p_smdb,
+								 &outstanding_count);
 				break;
 #endif
 			default:
@@ -1092,12 +1102,8 @@ ssa_log(SSA_LOG_DEFAULT, "SSA_DB_UPDATE_READY from access with outstanding count
 #ifndef SIM_SUPPORT
 				case SSA_DB_UPDATE_READY:
 ssa_log(SSA_LOG_DEFAULT, "SSA_DB_UPDATE_READY on pfds[%u] with outstanding count %d\n", i, outstanding_count);
-					if (outstanding_count > 0) {
-						if (--outstanding_count == 0) {
-							core_extract_db(p_osm);
-							ssa_extract_db_update(ssa_db_diff->p_smdb);
-						}
-					}
+					ssa_extract_update_ready_process(p_osm, ssa_db_diff->p_smdb,
+									 &outstanding_count);
 					break;
 #endif
 				default:

commit bb2785e35a527872ea00ade7b7c8f525de41523a

core.c: Fix ssa_extract_update_ready_process implementation
Broken by and since commit 9ab787a7d9670825ce8d628993993a40c65da2bd
plugin/core.c: Move duplicated code into a single method

This is since core_extract_db modifies ssa_db_diff->p_smdb
so that can't be passed into the routine (prior to
call to core_extract_db).

Signed-off-by: Hal Rosenstock <hal@mellanox.com>

plugin/src/core.c
@@ -942,13 +942,12 @@ static void core_extract_db(osm_opensm_t *p_osm)
 
 #ifndef SIM_SUPPORT
 static void ssa_extract_update_ready_process(osm_opensm_t *p_osm,
-					     struct ref_count_obj *p_smdb,
 					     int *outstanding_count)
 {
 	if (*outstanding_count > 0) {
 		if (--(*outstanding_count) == 0) {
 			core_extract_db(p_osm);
-			ssa_extract_db_update(p_smdb);
+			ssa_extract_db_update(ssa_db_diff->p_smdb);
 		}
 	}
 }
@@ -1077,7 +1076,6 @@ else ssa_log(SSA_LOG_DEFAULT, "extract event with extract already pending\n");
 			case SSA_DB_UPDATE_READY:
 ssa_log(SSA_LOG_DEFAULT, "SSA_DB_UPDATE_READY from access with outstanding count %d\n", outstanding_count);
 				ssa_extract_update_ready_process(p_osm,
-								 ssa_db_diff->p_smdb,
 								 &outstanding_count);
 				break;
 #endif
@@ -1110,7 +1108,6 @@ ssa_log(SSA_LOG_DEFAULT, "SSA_DB_UPDATE_READY from access with outstanding count
 				case SSA_DB_UPDATE_READY:
 ssa_log(SSA_LOG_DEFAULT, "SSA_DB_UPDATE_READY on pfds[%u] with outstanding count %d\n", i, outstanding_count);
 					ssa_extract_update_ready_process(p_osm,
-									 ssa_db_diff->p_smdb,
 									 &outstanding_count);
 					break;
 #endif