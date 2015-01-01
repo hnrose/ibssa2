9f0a19b09fd3f04dd5581594be17433d0a3fea05
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-01-01 16:49:59 +0200

    ssa_signal_handler.c: Use default signal handler for SIGABRT
    This could help in investigation why sometimes core dumps are
    not created.
    
    Signed-off-by: Sasha Kotchubievsky <sashakot@mellanox.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- shared/ssa_signal_handler.c
+++ shared/ssa_signal_handler.c
@@ -347,8 +347,6 @@ int ssa_set_ssa_signal_handler()
 		return 1;
 	if (sigaction(SIGILL,  &action, NULL) != 0)
 		return 1;
-	if (sigaction(SIGABRT, &action, NULL) != 0)
-		return 1;
 
 	return 0;
 }
