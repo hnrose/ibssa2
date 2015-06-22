e58586dbf14f6c6bb3286824afacea0dafa4baec
Hal Rosenstock <hal@mellanox.com>
2015-06-22 21:09:18 +0300

    acm_util.h: Cosmetic formatting change
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

--- acm/src/acm_util.h
+++ acm/src/acm_util.h
@@ -52,8 +52,8 @@ int acm_if_get_pkey(char *ifname, uint16_t *pkey);
 int acm_if_get_sgid(char *ifname, union ibv_gid *sgid);
 
 typedef void (*acm_if_iter_cb)(char *ifname, union ibv_gid *gid, uint16_t pkey,
-				uint8_t addr_type, uint8_t *addr, size_t addr_len,
-				char *ip_str, void *ctx);
+			       uint8_t addr_type, uint8_t *addr, size_t addr_len,
+			       char *ip_str, void *ctx);
 int acm_if_iter_sys(acm_if_iter_cb cb, void *ctx);
 
 #endif /* ACM_IF_H */
