b079901fb3240a727866ead17860f61353ae1967
Ilya Nelkenbaum <ilyan@mellanox.com>
2015-06-22 15:50:43 +0300

    tests/utils: Add prdb2hosts utility
    prdb2hosts utility converts PRDB to ibacm hosts file.
    For each record in path record table, we will have 3
    records in generated host file, 1 for each type of
    network addreses: IPv4, IPv6 and NAME.
    
    Above addresses will be generated as follows:
    
    IPv4 - simple counter for each address octet that will
           start from 0, and will be incremented each time
           a new record is added.
    
    IPv6 - will be combined of some constant subnet prefix
           and a guid identical to destination guid.
    
    NAME - prefix 'host' combined with destination LID in
           host order (example: host357).
    
    Signed-off-by: Ilya Nelkenbaum <ilyan@mellanox.com>
    Signed-off-by: Hal Rosenstock <hal@mellanox.com>

---
+++ tests/utils/prdb2hosts.c
@@ -0,0 +1,227 @@
+/*
+ * Copyright 2015 Mellanox Technologies LTD. All rights reserved.
+ *
+ * This software is available to you under the terms of the
+ * OpenIB.org BSD license included below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <infiniband/ssa_db_helper.h>
+#include <infiniband/ssa_db.h>
+#include <infiniband/ssa_prdb.h>
+#include <ssa_log.h>
+#include <common.h>
+
+static char log_file[128]	= "/var/log/prdb2hosts.log";
+static uint64_t	subnet_prefix	= 0xfe80000000000000;
+
+union gid {
+        uint8_t                 raw[16];
+        struct {
+                uint64_t        subnet_prefix;
+                uint64_t        interface_id;
+        } global;
+};
+
+static void print_usage(FILE* file,const char* name)
+{
+	fprintf(file, "Usage: %s [-m prdb mode] [-s subnet prefix] "
+		      "[-o hosts output file] prdb directory\n", name);
+	fprintf(file, "PRDB input mode:\n");
+	fprintf(file, "\tb - Binary (default)\n");
+	fprintf(file, "\td - Debug\n");
+}
+
+static int is_dir_exist(const char* path)
+{
+	DIR *dir = opendir(path);
+
+	if (dir) {
+		closedir(dir);
+		dir = NULL;
+		return 1;
+	}
+	return 0;
+}
+
+static int gen_hosts(struct ssa_db *prdb, const char *hosts_file)
+{
+	FILE *fd;
+	struct ep_pr_tbl_rec *tbl, *rec;
+	char buf[120];
+	uint8_t ipv4[4] = { 1, 1, 1, 1 };
+	union gid gid;
+	int j;
+	uint64_t i, pr_cnt;
+	uint16_t lid;
+
+	fd = fopen(hosts_file, "w");
+	if (!fd) {
+		fprintf(stderr, "unable to open hosts file (%s) for writing\n",
+			hosts_file);
+		return -1;
+	}
+
+	tbl = (struct ep_pr_tbl_rec *) prdb->pp_tables[SSA_PR_TABLE_ID];
+	pr_cnt = ntohll(prdb->p_db_tables[SSA_PR_TABLE_ID].set_count);
+
+	fprintf(fd, "#\n");
+	fprintf(fd, "# InfiniBand Communication Management Assistant for clusters hosts file\n");
+	fprintf(fd, "#\n");
+	fprintf(fd, "# Number of records:\t%lu\n", pr_cnt * 3);
+	fprintf(fd, "# Subnet prefix used:\t0x%" PRIx64 "\n", subnet_prefix);
+	fprintf(fd, "#\n\n");
+
+	gid.global.subnet_prefix = htonll(subnet_prefix);
+
+	for (i = 0; i < pr_cnt; i++) {
+		rec = tbl + i;
+
+		lid = ntohs(rec->lid);
+		gid.global.interface_id = rec->guid;
+		inet_ntop(AF_INET6, gid.raw, buf, sizeof(buf));
+
+		fprintf(fd, "%u.%u.%u.%u %s\n",
+			ipv4[0], ipv4[1], ipv4[2], ipv4[3], buf);
+		fprintf(fd, "%s %s\n", buf, buf);
+		fprintf(fd, "host%u %s\n", lid, buf);
+
+		for (j = sizeof(ipv4) - 1; j >= 0; j--) {
+			if (++ipv4[j])
+				break;
+		}
+	}
+
+	fclose(fd);
+
+	return 0;
+}
+
+int main(int argc,char *argv[])
+{
+	enum ssa_db_helper_mode ssa_db_mode = SSA_DB_HELPER_STANDARD;
+	char *input_path = NULL, *hosts_file = NULL;
+	struct ssa_db *p_ssa_db = NULL;
+	char *endptr;
+	unsigned long int tmp;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "m:o:s:h?")) != -1) {
+		switch (opt) {
+		case 'm':
+			if (optarg[0] == 'b') {
+				ssa_db_mode = SSA_DB_HELPER_STANDARD;
+			} else if (optarg[0] == 'd') {
+				ssa_db_mode = SSA_DB_HELPER_DEBUG;
+			} else {
+				print_usage(stdout, argv[0]);
+				return 0;
+			}
+			break;
+		case 'o':
+			hosts_file = optarg;
+			break;
+		case 's':
+			tmp = strtoull(optarg, &endptr, 0);
+			if (endptr == optarg ||
+			    (errno == ERANGE && tmp == ULONG_MAX)) {
+				fprintf(stderr,
+					"invalid subnet prefix specifed (%s), "
+					"using default one 0x%" PRIx64 "\n",
+					optarg, subnet_prefix);
+			} else {
+				subnet_prefix = (uint64_t) tmp;
+			}
+			break;
+		case '?':
+		case 'h':
+			print_usage(stdout, argv[0]);
+			return 0;
+			break;
+		default: /* '?' */
+			if(isprint (optopt))
+				fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+			else
+				fprintf(stderr, "Unknown option character `\\x%x'.\n",
+					optopt);
+			print_usage(stderr, argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (argc == optind ) {
+		fprintf(stderr, "Not enough input arguments\n");
+		print_usage(stderr, argv[0]);
+		exit(EXIT_FAILURE);
+	} else if (argc == (optind + 1)) {
+		input_path = argv[optind];
+	} else {
+		fprintf(stderr, "Too much input arguments\n");
+		print_usage(stderr, argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	if (!input_path || !is_dir_exist(input_path)) {
+		fprintf(stderr, "Input directory does not exist: %s\n", input_path);
+		print_usage(stderr, argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	if (!hosts_file || !strlen(hosts_file)) {
+		fprintf(stderr, "Invalid output hosts file specified\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Input PRDB path: %s\n", input_path);
+	printf("Output hosts file: %s\n", hosts_file);
+
+	ssa_open_log(log_file);
+
+	ssa_set_ssa_signal_handler();
+
+	p_ssa_db = ssa_db_load(input_path, ssa_db_mode);
+	if (!p_ssa_db)
+		exit(EXIT_FAILURE);
+
+	if (gen_hosts(p_ssa_db, hosts_file)) {
+		fprintf(stderr, "ERROR - unable to generate hosts file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	ssa_db_destroy(p_ssa_db);
+	ssa_close_log();
+
+	return 0;
+}
